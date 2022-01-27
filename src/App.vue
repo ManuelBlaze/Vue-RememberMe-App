ea2c1d544e1c8c9dc4a10cfc3c667e8cdc8b4d3a
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-27 17:06:13 -0500

    Add Styles

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul>
+    <resource-item
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import ResourceItem from './components/resources/ResourceItem.vue';
+
+export default {
+  components: {
+    ResourceItem,
+  },
+  data() {
+    return {
+      resources: [
+        {
+          id: 0,
+          title: 'Official Guide',
+          description: 'The official VueJS documentation',
+          link: 'https://v3.vuejs.org/',
+        },
+        {
+          id: 1,
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
